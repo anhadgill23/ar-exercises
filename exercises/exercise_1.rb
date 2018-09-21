57b88fd8aa4667cd97b122376f75598e9817377c
Anhad Gill <anhadgill@Anhads-MacBook-Pro-2.local>
2018-09-21 09:26:34 -0700

    refactoring.

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,13 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+store_2 = Store.new
+store_2.name = "Richmond"
+store_2.annual_revenue = "1260000"
+store_2.mens_apparel = false
+store_2.womens_apparel = true
+store_2.save!
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
